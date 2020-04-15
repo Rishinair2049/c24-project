3c9d71c90154ca9ba3859564bf05a4a02b03c0db
Rishinair2049 <59763668+Rishinair2049@users.noreply.github.com>
2020-04-15 15:48:34 +0530

    Add files via upload

---
+++ Tanker.js
@@ -0,0 +1,29 @@
+class Tanker {
+
+  constructor(x, y, width, height) {
+
+      var options = {
+      isStatic: true
+     }
+
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+
+    this.image = loadImage("tank.png");
+    World.add(world, this.body);
+
+  };
+
+  display(){
+
+    // Draw the tanker the way you like.
+    // You could also use an image if you want a specific look
+
+    var pos =this.body.position;
+    imageMode(CENTER);
+    fill("white");
+    image(this.image, pos.x, pos.y, this.width, this.height);
+
+  };
+}
